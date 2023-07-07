d8261f7559ff3502f0365a067d481ab3b6865378
Farjana Akter <fakter@xpanxion.com>
2023-07-06 22:25:24 -0400

    update file

--- server/server.py
+++ server/server.py
@@ -1,16 +1,23 @@
 from flask import Flask ,jsonify
 
+from flask_cors import CORS
+
+
 app = Flask(__name__)
+
+CORS(app)
+
 @app.route("/api/home",methods=['GET'])
 def return_home():
    return jsonify({
       
-"message":"Hello world From Farjana"
+"message":"Hello world From Farjana Akter",
+'people':['jack','marry','barry','Farjana','Aariz']
 
    }
    )
 
 if __name__ == "__main__":
-  app.run(debug = True)
+  app.run(debug = True, port=8080)
 
 
